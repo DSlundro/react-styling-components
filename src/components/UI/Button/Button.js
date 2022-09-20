ae9ad6b41af31b2773c9418b479faaf6fb7753d2
DSlundro <dragan.savic95@outlook.it>
2022-09-20 19:37:20 +0200

    final commit

---
+++ src/components/UI/Button/Button.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+import './Button.css';
+
+import styled from 'styled-components';
+
+const Button = styled.button`
+
+`;
+
+
+
+/* const Button = props => {
+  return (
+    <button type={props.type} className="button" onClick={props.onClick}>
+      {props.children}
+    </button>
+  );
+}; */
+
+export default Button;
